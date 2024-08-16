c2b3fbe7057490e5b7a6dab53c5d170a7df9fb6f
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-16 15:49:41 +0300

    The useCallback Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useRef, useState, useCallback} from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -58,9 +58,9 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+        prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     setModalIsOpen(false);
 
@@ -69,7 +69,7 @@ function App() {
         'selectedPlaces',
         JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
