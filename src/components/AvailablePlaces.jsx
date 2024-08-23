08811a7175dbb12e8e9e1a0ff57b8620bf3f0016
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-23 10:40:37 +0300

    fetch request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [avaliablePlaces, setAvaliablePlaces] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/places')
+            .then((response) => {
+                return response.json();
+            })
+            .then((resData) => {
+                setAvaliablePlaces(resData.places);
+            });
+    }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={avaliablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
